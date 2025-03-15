b407ae32862be2309351c2063ee650690b9cc00d
YalvacMustafa <zapza574@gmail.com>
2025-03-16 00:56:10 +0300

    hatalar düzeltildi.

--- routers/auth.js
+++ routers/auth.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { register, login, logout } = require('../controllers/auth');
-
+const { getAccessToRoute } = require('../middlewares/authorization/auth')
 router.post('/register', register);
 router.post('/login', login);
-router.get('logout', logout)
+router.get('/logout', getAccessToRoute, logout)
 module.exports = router;
