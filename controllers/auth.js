4bfa442a1bdca0df5232f13d13e14808e7c8368f
YalvacMustafa <zapza574@gmail.com>
2025-03-15 21:12:46 +0300

    login ve gitignore eklendi

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const customerror = require('../helpers/error/customerror');
 const { sendJwtToClient } = require('../helpers/authorization/tokenhelpers');
+const { validateUserInput, comparePassword } = require('../helpers/input/inputhelpers');
 
 const register = async (req, res, next) => {
     const { name, email, password } = req.body;
@@ -12,4 +13,15 @@ const register = async (req, res, next) => {
     sendJwtToClient(user, res);
 };
 
-module.exports = { register }
+const login = async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!validateUserInput(email, password)){
+        return next(new customerror('Lütfen girdilerinizi kontrol ediniz.', 400))
+    }
+    const user = await User.findOne({ email }).select('+password');
+    if (!comparePassword(password, user.password)){
+        return next(new customerror('Parola yanlış', 400))
+    }
+    sendJwtToClient(user, res)
+};
+module.exports = { register, login }
