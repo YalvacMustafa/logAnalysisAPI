81da6fdefd9cd7436c062ea0ea9ea0e1729ec6b3
YalvacMustafa <zapza574@gmail.com>
2025-03-17 15:32:51 +0300

    metada object hatası düzeltildi.

--- models/log.js
+++ models/log.js
@@ -5,7 +5,7 @@ const logSchmea = new Schema({
     message: { type: String, required: true },
     level: { type: String, enum: ['info', 'warn', 'error'], required: true },
     timeStamp: { type: Date, default: Date.now },
-    metadata: { type: String, required: true },
+    metadata: { type: Object },
     insights: { type: Object, default: null }
 })
 
